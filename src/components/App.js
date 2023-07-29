9b91e2cd562f752e5da9ea89ecb2e92994597f95
logkyung <aebong54@gmail.com>
2023-07-29 21:11:23 +0900

    feat: add '더보기' button using pagination

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,55 @@
+import { getReviews } from "../api";
 import ReviewList from "./ReviewList";
-import mockItems from "../mock.json";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [items, setItems] = useState(mockItems);
+  const [items, setItems] = useState([]);
   const [order, setOrder] = useState("createdAt");
+  const [offset, setOffset] = useState(0);
+  const [hasNext, setHasNext] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [loadingError, setLoadingError] = useState(null);
+
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
-  const handleNewestClick = () => setOrder('createdAt');
-  const handleBestClick = () => setOrder('rating');
+  const handleNewestClick = () => setOrder("createdAt");
+
+  const handleBestClick = () => setOrder("rating");
+
   const handleDelete = (id) => {
     const nextItems = items.filter((item) => item.id !== id);
     setItems(nextItems);
-  }
+  };
+
+  const handleLoad = async (options) => {
+    let result;
+    try {
+      setIsLoading(true);
+      setLoadingError(null);
+      result = await getReviews(options);
+    } catch (error) {
+      setLoadingError(error);
+      return;
+    } finally {
+      setIsLoading(false);
+    }
+    const { reviews, paging } = result;
+    if (options.offset === 0) {
+      setItems(reviews);
+    } else {
+      setItems((prevItems) => [...prevItems, ...reviews]);
+    }
+    setOffset(options.offset + options.limit);
+    setHasNext(paging.hasNext);
+  };
+
+  const handleLoadMore = () => {
+    handleLoad({ order, offset, limit: 6 });
+  };
+
+  useEffect(() => {
+    handleLoad({ order, offset: 0, limit: 6 });
+  }, [order]);
 
   return (
     <div>
@@ -21,6 +58,12 @@ function App() {
         <button onClick={handleBestClick}>평점순</button>
       </div>
       <ReviewList items={sortedItems} onDelete={handleDelete} />
+      {hasNext && (
+        <button disabled={isLoading} onClick={handleLoadMore}>
+          더보기
+        </button>
+      )}
+      {loadingError?.message && <span>{loadingError.message}</span>}
     </div>
   );
 }
