a91c5b9a971cc92e1a5fd178214824bdab9d4081
logkyung <aebong54@gmail.com>
2023-07-30 19:03:22 +0900

    feat: add FileInput component

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,29 +1,48 @@
 import { useState } from "react";
 import "./ReviewForm.css";
+import FileInput from "./FileInput";
 
 function ReviewForm() {
-  const [title, setTitle] = useState("");
-  const [rating, setRating] = useState(0);
-  const [content, setContent] = useState("");
+  const [values, setValues] = useState({
+    title: "",
+    rating: 0,
+    content: "",
+    imgFile: null,
+  });
 
-  const handleTitleChange = (e) => {
-    setTitle(e.target.value);
+  const handleChange = (name, value) => {
+    setValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
+  }
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    handleChange(name, value);
   };
 
-  const handleRatingChange = (e) => {
-    const nextRating = Number(e.target.value) || 0;
-    setRating(nextRating);
-  };
-
-  const handleContentChange = (e) => {
-    setContent(e.target.value);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(values);
   };
 
   return (
-    <form className="ReviewForm">
-      <input value={title} onChange={handleTitleChange}></input>
-      <input type="number" value={rating} onChange={handleRatingChange}></input>
-      <textarea value={content} onChange={handleContentChange}></textarea>
+    <form className="ReviewForm" onSubmit={handleSubmit}>
+      <FileInput name="imgFile" value={values.imgFile} onChange={handleChange} />
+      <input name="title" value={values.title} onChange={handleInputChange}></input>
+      <input
+        name="rating"
+        type="number"
+        value={values.rating}
+        onChange={handleInputChange}
+      ></input>
+      <textarea
+        name="content"
+        value={values.content}
+        onChange={handleInputChange}
+      ></textarea>
+      <button type="submit">확인</button>
     </form>
   );
 }
